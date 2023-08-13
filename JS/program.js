2e860835f319104d574ed99f07799a4a25dc1b17
Ziyadxan Dovletov <ziyadxan.dovletov06@gmail.com>
2023-08-13 06:20:32 -0700

    bax

---
+++ JS/program.js
@@ -0,0 +1,30 @@
+let cardDiv = document.querySelector(".cards")
+
+function getData(){
+    fetch("../db/Exams.json")
+    .then(res => res.json())
+    .then(data => {
+        for(let user of data){
+            console.log(data)
+            cardDiv += `
+            <div class="card">
+                <img class="student-img" src="${user.userImage}" alt="">
+
+                <div class="name">
+                    <p>${user.questions}</p>
+                    <p>${user.fullName}</p>
+                </div>
+
+                <p class="card-title">${user.examName}</p>
+                <p class="right">${user.price}</p>
+                <div class="card-buttons">
+                    <a href="#">Teste basla</a>
+                    <a href="#">Daha etrafli</a>
+                </div>
+            </div>
+            `
+        }
+    })
+}
+
+getData();
