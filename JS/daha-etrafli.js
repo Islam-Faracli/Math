753af16e170da7343f506c59b807ae21a690b22d
İslam Fərəcli <slmfrcl2004@gmail.com>
2023-08-16 15:38:49 +0400

    mode and connections between pages are added

--- JS/daha-etrafli.js
+++ JS/daha-etrafli.js
@@ -4,10 +4,8 @@ options.forEach((element) => {
     element.addEventListener("click", ()=>{
         options.forEach((items)=>{
             items.classList.remove("selected");
-            
         })
         element.classList.add("selected");
-        
     })
 })
 
@@ -34,24 +32,49 @@ fetch("/db/reytinq.json")
 
 let body = document.querySelector("body");
 let toDark = document.querySelector(".mode-box");
-let p=document.querySelectorAll("p");
-let h2=document.querySelectorAll("h2");
-let span=document.querySelectorAll("span");
-toDark.addEventListener("click", ()=>{
-    toDark.classList.toggle("toDarkMode");
-    setTimeout(()=>{
+let p = document.querySelectorAll("p");
+let h2 = document.querySelectorAll("h2");
+let span = document.querySelectorAll("span");
 
+toDark.addEventListener("click", () => {
+    toDark.classList.toggle("toDarkMode");
+    setTimeout(() => {
         body.classList.toggle("darhTheme");
-        p.forEach((item)=>{
+        p.forEach((item) => {
             item.classList.toggle("toDarkMode");
-        })
-        h2.forEach((item)=>{
+        });
+        h2.forEach((item) => {
             item.classList.toggle("toDarkMode");
-        })
-        span.forEach((item)=>{
+        });
+        span.forEach((item) => {
             item.classList.toggle("toDarkMode");
-        })
+        });
         reytinq.classList.toggle("toDarkMode");
-    },250)
-    
-})
+
+        if (toDark.classList.contains("toDarkMode")) {
+            localStorage.setItem("toDarkMode", true);
+        } else {
+            localStorage.setItem("toDarkMode", false);
+        }
+    }, 250);
+});
+
+window.onload = function () {
+    let localusers = localStorage.getItem("toDarkMode");
+    if (localusers === "true") { // Check for string "true"
+        toDark.classList.toggle("toDarkMode");
+        setTimeout(() => {
+            body.classList.toggle("darhTheme"); // Fixed typo here
+            p.forEach((item) => {
+                item.classList.toggle("toDarkMode");
+            });
+            h2.forEach((item) => {
+                item.classList.toggle("toDarkMode");
+            });
+            span.forEach((item) => {
+                item.classList.toggle("toDarkMode");
+            });
+            reytinq.classList.toggle("toDarkMode");
+        }, 0);
+    }
+};
